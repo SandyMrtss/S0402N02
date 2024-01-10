716cdd5807d36060d5810fd5e5eb706224da3e04
SandyMrtss <bebesubi2@hotmail.com>
2024-01-10 10:52:02 +0100

    Adds more exceptions to handler

--- src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s04/t02/n02/exceptions/ExceptionHandlerAdvice.java
+++ src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s04/t02/n02/exceptions/ExceptionHandlerAdvice.java
@@ -2,7 +2,9 @@ package cat.itacademy.barcelonactiva.martos.sandra.s04.t02.n02.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.*;
+import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +16,7 @@ public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex){
-        return ResponseEntity.badRequest().body("Input not valid");
+        return ResponseEntity.badRequest().body("Input not valid\n" + ex.getMessage());
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
@@ -22,8 +24,18 @@ public class ExceptionHandlerAdvice {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No element found with id");
     }
 
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity<String> handlePrimaryKeyViolation(SQLException ex){
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handlePrimaryKeyViolation(DataIntegrityViolationException ex){
         return ResponseEntity.status(HttpStatus.CONFLICT).body("Element already exists");
+    }
+
+    @ExceptionHandler(HttpMessageConversionException.class)
+    public ResponseEntity<String> handleInvalidInput(HttpMessageConversionException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Incorrect data type");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> hadleGeneralException(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something unexpected went wrong\n" + ex.getMessage());
     }
 }
